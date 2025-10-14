45260df9aeb17319109faac00ed887e264870e4a
Tali <talicabana@gmail.com>
2025-10-13 22:03:58 -0300

    Dividimos la lógica de productos en el controlador

---
+++ src/controllers/productos.controllers.js
@@ -0,0 +1,5 @@
+export const prueba = (req, res) => {
+    console.log('Desde el controlador de prueba');
+
+    res.send('Prueba desde el controlador');
+}
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,4 +1,5 @@
 import {Router} from 'express';
+import { prueba } from '../controllers/productos.controllers.js';
 
 /*
 GET
@@ -9,13 +10,6 @@ DELET
 
 const router = Router();
 
-router.route('/test').get((req, res) => {
-    console.log('Desde el controlador de prueba');
-
-    res.send('Prueba desde el controlador');
-
-
-
-})
+router.route('/test').get(prueba)
 
 export default router;
