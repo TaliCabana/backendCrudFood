9df77a70293adfacfe48311ba62d642978420086
Tali <talicabana@gmail.com>
2025-10-15 22:23:25 -0300

    Creamos el producto en la BD - Post

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,9 +1,21 @@
+import Producto from '../models/productos.js';
+
 export const prueba = (req, res) => {
     console.log('Desde el controlador de prueba');
 
     res.send('Prueba desde el controlador');
 }
 
-export const crearProducto = (req, res) => {
-    res.send('Aquí tenemos que crear el producto');
+export const crearProducto = async (req, res) => {
+    try{
+        // 1- Verificar que lleguen los datos validados
+        // 2- Pedir al modelo Producto crear el objeto en la BD
+        console.log(req.body);
+        const productoNuevo = new Producto(req.body);
+        await productoNuevo.save();
+        res.status(201).json("El producto fue creado exitosamente");
+    } catch(error){
+        console.error(error);
+        res.this.status(500).json({ mensaje: 'Ocurrió un error al crear el producto' });
+}
 }
