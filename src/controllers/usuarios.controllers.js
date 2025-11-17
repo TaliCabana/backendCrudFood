4b34e167c2a4cec860b7f286de5e7902ba40120d
Tali <talicabana@gmail.com>
2025-11-16 22:26:10 -0300

    Hasheamos el password del usuario al momento de crearlo y agregamos listarUsuario

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -1,15 +1,32 @@
 import { body } from "express-validator";
 import Usuario from "../models/usuario.js";
+import bcrypt from "bcrypt";
 
 export const crearUsuario = async (req, res) => {
   try {
+    const saltos = bcrypt.genSaltSync(10);
+    const passwordEncriptado = bcrypt.hashSync(req.body.password, saltos);
+    req.body.password = passwordEncriptado;
     const usuarioNuevo = new Usuario(req.body);
+
     await usuarioNuevo.save();
-    res.status(201).json({mensaje: "Usuario creado correctamente"})
+    res.status(201).json({ mensaje: "Usuario creado correctamente" });
   } catch (error) {
     console.error(error);
     res
       .status(500)
       .json({ mensaje: "Ocurrió un error, no se pudo crear el usuario." });
+  }
+};
+
+export const listarUsuarios = async (req, res) => {
+  try {
+    const usuarios = await Usuario.find();
+    res.status(200).json(usuarios);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ mensaje: "Ocurrió un error, no se pudo listar los usuarios." });
   }
 };
--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { crearUsuario } from "../controllers/usuarios.controllers.js";
+import { crearUsuario, listarUsuarios } from "../controllers/usuarios.controllers.js";
 
 
 const router = Router();
 
-router.route("/").post(crearUsuario); // Cuando haga un post, que invoque al controlador que aun no tengo 
+router.route("/").post(crearUsuario).get(listarUsuarios); // Cuando haga un post, que invoque al controlador que aun no tengo 
 
 export default router;
