bd081f1623e6eaf70757ff6a642772705f799c8c
Tali <talicabana@gmail.com>
2025-11-16 23:25:29 -0300

    Realizamos el login

--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { crearUsuario, listarUsuarios } from "../controllers/usuarios.controllers.js";
+import { crearUsuario, listarUsuarios, login } from "../controllers/usuarios.controllers.js";
 
 
 const router = Router();
 
-router.route("/").post(crearUsuario).get(listarUsuarios); // Cuando haga un post, que invoque al controlador que aun no tengo 
+router.route("/").post(crearUsuario).get(listarUsuarios); // Cuando haga un post, que invoque al controlador que aun no tengo (---)
+router.route('/login').post(login)
+
 
 export default router;
